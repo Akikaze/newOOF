1249c522d11819a219834611008569980b583fa0
DPF1 <david.feraud@nist.gov>
2016-06-23 12:03:18 -0400

    command parser system : not sure that it's working, need to be tested

---
+++ data/command_parser.hpp
@@ -0,0 +1,33 @@
+#ifndef COMMAND_PARSER_HPP
+#define COMMAND_PARSER_HPP
+
+#include "oofobject.hpp"
+
+template < typename T >
+class CommandParser
+: public OOFOBJECT< CommandParser >
+{
+	public :
+		
+	
+	protected :
+		
+	
+	private :
+		
+	
+} ;
+
+class CommandParser< void >
+: public OOFOBJECT< CommandParser< void > >
+{
+	public :
+		
+	
+	protected :
+		
+	
+	private :
+		
+	
+} ;
